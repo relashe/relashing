fa5917b2289d4f422645cc34b8882129c3da4e84
Paciencia Zinga Canda <paciencia@relashe.com>
2020-01-19 22:18:37 +0000

    rollup and rimraf

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import babel from "rollup-plugin-babel";
+import postcss from "rollup-plugin-postcss";
+import reactSvg from "rollup-plugin-react-svg";
+import commonjs from "rollup-plugin-commonjs";
+import progress from "rollup-plugin-progress";
+import visualizer from "rollup-plugin-visualizer";
+
+export default {
+  input: "src/index.js",
+  output: {
+    file: "dist/lib.es.js",
+    format: "es",
+    sourcemap: true,
+  },
+  plugins: [
+    postcss({
+      minimize: true,
+      plugins: [],
+    }),
+    // reactSvg(),
+    babel({
+      exclude: "node_modules/**",
+    }),
+    commonjs(),
+    progress(),
+    visualizer({
+      filename: "./dist/stats.html",
+    }),
+  ],
+  external: ["react", "react-dom"],
+};
