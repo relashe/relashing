40138d6d0d5cd95cb3e1afb6bcb259211983f754
Paciencia Zinga Canda <paciencia@relashe.com>
2020-01-20 09:07:41 +0000

    colour name updates

--- src/core/colours/colours.stories.js
+++ src/core/colours/colours.stories.js
@@ -16,29 +16,29 @@ palettes
   .add("Core brand colours", () => (
     <div className="grid-x grid-margin-x grid-margin-y">
       <div className="cell screen768-6">
-        <ColorSwatch name="Relashe Blue" color="#173450" variable="get-color(blue)" />
+        <ColorSwatch name="Relashe Blue" color="#173450" variable="get-colour(brand-blue)" />
       </div>
       <div className="cell screen768-6">
-        <ColorSwatch name="Relashe Blue Light" color="#3C6183" variable="get-color(blue-light)" />
+        <ColorSwatch name="Relashe Blue Light" color="#3C6183" variable="get-colour(brand-blue-light)" />
       </div>
       <div className="cell screen768-6">
-        <ColorSwatch name="Relashe Blue Dark" color="#161724" variable="get-color(blue-dark)" />
+        <ColorSwatch name="Relashe Blue Dark" color="#161724" variable="get-colour(brand-blue-dark)" />
       </div>
       <div className="cell screen768-6">
-        <ColorSwatch name="Relashe Grey" color="#f0f0f0" variable="get-color(grey)" />
+        <ColorSwatch name="Relashe Grey" color="#f0f0f0" variable="get-colour(brand-grey)" />
       </div>
     </div>
   ))
   .add("Typography colours", () => (
     <div className="grid-x grid-margin-x grid-margin-y">
       <div className="cell screen768-6">
-        <ColorSwatch name="Title" color="#000000" variable="get-color(title)" />
+        <ColorSwatch name="Title" color="#000000" variable="get-colour(brand-title)" />
       </div>
       <div className="cell screen768-6">
-        <ColorSwatch name="Text" color="#000000" variable="get-color(text)" />
+        <ColorSwatch name="Text" color="#000000" variable="get-colour(brand-text)" />
       </div>
       <div className="cell screen768-6">
-        <ColorSwatch name="Link" color="currentColor" variable="get-color(link)" />
+        <ColorSwatch name="Link" color="currentColor" variable="get-colour(brand-link)" />
       </div>
     </div>
   ));
