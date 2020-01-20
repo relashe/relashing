dc87383b010442a308486e5fd742629f99493518
Paciencia Zinga Canda <paciencia@relashe.com>
2020-01-20 08:57:30 +0000

    component path fix

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-export { default as PageMain } from '.components/pages/PageMain/PageMain';
+export { default as PageMain } from './components/pages/PageMain/PageMain';
 export { default as BodyCopy } from './components/BodyCopy/BodyCopy';
 export { default as HomeBanner } from './components/HomeBanner/HomeBanner';
 export { default as PageBanner } from './components/PageBanner/PageBanner';
